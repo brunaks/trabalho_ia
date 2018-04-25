9951543af1af722c00281100278a0218df483060
bruna <brunak_s@hotmail.com>
2018-04-25 01:33:12 -0300

    Proccessing

--- db.py
+++ db.py
@@ -5,14 +5,23 @@ conn = psycopg2.connect("dbname='postgres' user='bruna'")
 
 for source_file in os.listdir("wordLemPoS"):
     file = open("wordLemPoS/" + source_file)
-    sentences = file.read().split('.')
-    for sentence in sentences:
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO trabalho_ia.sentences (text, source_file) VALUES (%s, %s)", (sentence, source_file))
-conn.commit()
-
-# cursor = conn.cursor()
-# cursor.execute("""SELECT * FROM trabalho_ia.sentences""")
-# rows = cursor.fetchall()
-# for row in rows:
-#     print row
+    sentence_id = 1
+    part = 1
+    for line in file.readlines()[1:]:
+        columns = line.split("\t")
+        text = columns[0]
+        lemma = columns[1]
+        tag = columns[2].replace('\r\n', '')
+        if text == '.':
+            sentence_id += 1
+            part = 1
+        else:
+            cursor = conn.cursor()
+            try:
+                cursor.execute("INSERT INTO trabalho_ia.sentences " +
+                               "(id, part, text, lemma, tag, source_file) " +
+                               "VALUES (%s, %s, %s, %s, %s, %s)", (sentence_id, part, text, lemma, tag, source_file))
+                conn.commit()
+            except:
+                conn = psycopg2.connect("dbname='postgres' user='bruna'")
+            part += 1
